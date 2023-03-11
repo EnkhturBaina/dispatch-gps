6a547f659a4e9fc867b47f8702773b3f98429dc5
enkhturBaina <eegii.111.gg@gmail.com>
2023-03-11 12:41:28 +0800

    zaswar

--- App.js
+++ App.js
@@ -1,10 +1,7 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
-import FlashMessage from "react-native-flash-message";
 
-//Screens
-import ChooseLocation from "./src/Screens/ChooseLocation";
-import Home from "./src/Screens/Home";
+import Home from "./src/screens/Home";
 
 export default function App() {
   const Stack = createStackNavigator();
@@ -13,9 +10,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="home" component={Home} />
-        <Stack.Screen name="chooseLocation" component={ChooseLocation} />
       </Stack.Navigator>
-      <FlashMessage position="top" />
     </NavigationContainer>
   );
 }
--- src/constants/imagePath.js
+++
@@ -1,6 +0,0 @@
-export default {
-    icCurLoc: require('../assets/images/Oval.png'),
-    icGreenMarker: require("../assets/images/greenMarker.png"),
-    greenIndicator: require("../assets/images/greenIndicator.png"),
-    icBike: require("../assets/images/bike.png"),
-}
