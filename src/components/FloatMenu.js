cf05b77731abaf779e07e136440ceb3acd6f1481
enkhturBaina <eegii.111.gg@gmail.com>
2023-03-12 20:03:00 +0800

    darkMode added

--- src/components/FloatMenu.js
+++ src/components/FloatMenu.js
@@ -1,9 +1,12 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import React, { useContext, useEffect } from "react";
 import { Icon } from "@rneui/base";
+import { DARK_MODE_BG_COLOR, DARK_MODE_LIGHT_COLOR } from "../constant";
+import MainContext from "../contexts/MainContext";
 
 const FloatMenu = () => {
+  const state = useContext(MainContext);
+
   return (
     <View
       style={{
@@ -11,22 +14,52 @@ const FloatMenu = () => {
         top: 30,
         width: "5%",
         flexDirection: "column",
+        backgroundColor: state.isDark ? DARK_MODE_BG_COLOR : "#fff",
+        zIndex: 999,
       }}
     >
       <TouchableOpacity style={styles.eachMenuContainer}>
-        <Icon type="feather" name="power" size={30} />
+        <Icon
+          type="feather"
+          name="power"
+          size={30}
+          color={state.isDark ? DARK_MODE_LIGHT_COLOR : DARK_MODE_BG_COLOR}
+        />
       </TouchableOpacity>
       <TouchableOpacity style={styles.eachMenuContainer}>
-        <Icon type="feather" name="plus" size={30} />
+        <Icon
+          type="feather"
+          name="plus"
+          size={30}
+          color={state.isDark ? DARK_MODE_LIGHT_COLOR : DARK_MODE_BG_COLOR}
+        />
       </TouchableOpacity>
       <TouchableOpacity style={styles.eachMenuContainer}>
-        <Icon type="feather" name="minus" size={30} />
+        <Icon
+          type="feather"
+          name="minus"
+          size={30}
+          color={state.isDark ? DARK_MODE_LIGHT_COLOR : DARK_MODE_BG_COLOR}
+        />
       </TouchableOpacity>
       <TouchableOpacity style={styles.eachMenuContainer}>
-        <Icon type="ionicon" name="earth-outline" size={30} />
+        <Icon
+          type="ionicon"
+          name="earth-outline"
+          size={30}
+          color={state.isDark ? DARK_MODE_LIGHT_COLOR : DARK_MODE_BG_COLOR}
+        />
       </TouchableOpacity>
-      <TouchableOpacity style={styles.eachMenuContainer}>
-        <Icon type="material-community" name="circle-half-full" size={30} />
+      <TouchableOpacity
+        style={styles.eachMenuContainer}
+        onPress={() => state.setIsDark(!state.isDark)}
+      >
+        <Icon
+          type="material-community"
+          name="circle-half-full"
+          size={30}
+          color={state.isDark ? DARK_MODE_LIGHT_COLOR : DARK_MODE_BG_COLOR}
+        />
       </TouchableOpacity>
     </View>
   );
